7ef04be24c70134a5f45de03c2439aa5c9c357e0
stefan4472 <stefan.kussmaul@gmail.com>
2015-07-29 09:19:24 -0400

    Initial Commit

---
+++ src/CLIUtil.java
@@ -0,0 +1,63 @@
+/**
+ * A java library offering various functions for use in CLI-based applications
+ * Functions include input/output, formatting, String manipulation and parsing,
+ * and trim functions.
+ */
+public class CLIUtil {
+
+    public static String getCharInput() {
+
+    }
+
+    public static String getWordInput() {
+
+    }
+
+    public static String getLineInput() {
+
+    }
+
+    public static String getTextInput() {
+
+    }
+
+    public static void print(String s) {
+
+    }
+
+    public static void printLn(String s) {
+
+    }
+
+    public static void printTb(String s) {
+
+    }
+
+    public static int printMenu(String[] menuItems) {
+
+    }
+
+    public static void printList(String[] listItems) {
+
+    }
+
+    public static String trimL(String s) {
+
+    }
+
+    public static String trimR(String s) {
+
+    }
+
+    public static String normalizeText(String s) {
+
+    }
+
+    public static String[] splitWords(String s) {
+
+    }
+
+    public static String[] splitLines(String s) {
+
+    }
+}
