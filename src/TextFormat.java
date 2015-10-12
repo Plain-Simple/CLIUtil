76897e4281c77c471cd8a0a82d088c18c48e4c23
stefan4472 <stefan.kussmaul@gmail.com>
2015-10-12 19:36:41 -0400

    Small Commenting and Codestyle Improvements

--- src/TextFormat.java
+++ src/TextFormat.java
@@ -3,7 +3,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * A java library for text output.
+ * A java library for formatting Strings that contain text.
  * Copyright(C) Plain Simple Apps 2015
  * Licensed under GPL GNU Version 3 (see license.txt)
  * See plain-simple.github.io for more information.
@@ -11,39 +11,39 @@ import java.util.regex.Pattern;
 public class TextFormat {
 
     /**
-     * Formats the String, limiting the length of the text body to the
-     * specified column width. If the String is longer than the specified
+     * Formats specified String, limiting the length of each line of text to
+     * the specified column width. If the String is longer than the specified
      * column width it will be printed on more than one line.
-     * This function assumes it is printing text and will not split a word
-     * over a line unless the length of the word is longer than the column
-     * width.
+     * This function will not split a word over a line unless the length of
+     * the word is longer than the column width. Whitespace is preserved.
      *
-     * @param text String to be printed
+     * @param text String to be formatted
      * @param columnWidth max width of a line of text
      */
     public final static String formatWidth(String text, int columnWidth) { // todo: is this just leftJustify()?
-        /* Remove linebreaks from s (it will be reformatted with linebreaks) */
+        // temporarily remove tabs and line breaks
         text = text.replace("\\n|\\r", "");
         text = text.replace("\t", "     ");
 
+        // will store formatted text in lines
         ArrayList<String> lines = new ArrayList<>();
+
+        // split text
         ArrayList<String> white_space = new ArrayList<>();
         ArrayList<String> word_tokens = new ArrayList<>();
         splitWords(text, white_space, word_tokens);
 
-        /* Loop breaks first whitespace token into lines of whitespace
-         * if it is longer than columnWidth */
+        /* If first whitespace token is longer than columnWidth, break it up */
         while(white_space.get(0).length() > columnWidth) {
-            /* Add a full line of whitespace. Remaining whitespace is put on next line */
             lines.add(cloneString(" ", columnWidth) + "\n");
             white_space.set(0, white_space.get(0).substring(columnWidth));
         }
 
-        /* Add whitespace of length less than columnWidth */
+        /* Add (remaining) whitespace from first token */
         lines.add(white_space.get(0));
 
         /* Remove first index of white_space, allowing us to look at the rest of the
-         * String as pairs of a word followed by whitespace, not the other way around */
+         String as pairs of a word followed by whitespace, not the other way around */
         white_space.remove(0);
 
         int current_line = lines.size() - 1;
@@ -53,7 +53,7 @@ public class TextFormat {
             while (word_tokens.get(i).length() > columnWidth) {
                 String current_line_copy = lines.get(current_line);
                 /* Substring word token to the length of space left in the line
-                 * and add it to the current line */
+                 and add it to the current line */
                 lines.set(current_line, lines.get(current_line) +
                         word_tokens.get(i).substring(0, columnWidth - lines.get(current_line).length()) + "\n");
                 word_tokens.set(i, word_tokens.get(i).substring(columnWidth - current_line_copy.length()));
@@ -71,7 +71,7 @@ public class TextFormat {
             }
 
             /* If whitespace fits on this line, add it. If not, ignore it and create a new line */
-            if(lines.get(current_line).length() + white_space.get(i).length() <= columnWidth) {
+            if(lines.get(current_line).length() + white_space.get(i).length() <= columnWidth) { // todo: preserve whitespace
                 lines.set(current_line, lines.get(current_line) + white_space.get(i));
             } else {
                 lines.set(current_line, lines.get(current_line) + "\n");
@@ -93,15 +93,16 @@ public class TextFormat {
      * tokens. Note: the String will always start and end with "whitespace",
      * even if the whitespace is a String of length zero. This means that, with
      * the exception of the first element of whitespace, each word is followed
-     * by whitespace.
+     * by whitespace. Note: passed Lists will be reset.
      *
      * @param text String to split
-     * @param whiteSpace ArrayList containing whiteSpace tokens
+     * @param whiteSpace ArrayList containing whitespace tokens
      * @param wordTokens ArrayList containing word tokens
      */
-    private final static void splitWords(String text, ArrayList<String> whiteSpace, ArrayList<String> wordTokens) {
+    private final static void splitWords(String text, ArrayList<String> whiteSpace, ArrayList<String> wordTokens) { // todo: List, not ArrayList
+        whiteSpace = new ArrayList<>();
+        wordTokens = new ArrayList<>();
         int last_index = 0;
-
         Pattern find_words = Pattern.compile("\\S+");
         Matcher matcher = find_words.matcher(text);
 
@@ -123,36 +124,38 @@ public class TextFormat {
 
 
     /**
-     * Generates a String by concatenating toClone with itself
-     * n times. If either parameter is null, the function returns
-     * null. If toClone equals "" or n is equal to or less than 0,
-     * an empty String is returned.
+     * Generates a String by concatenating toClone with itself n times. Returns
+     * null if toClone is null, and returns an empty String if toClone equals ""
+     * or n is equal to/less than zero.
      *
      * @param toClone String to be "cloned"
-     * @param n number of times for @toClone to be repeated
-     * @return String composed of @toClone repeated @n times
+     * @param n number of times for toClone to be repeated
+     * @return String composed of toClone repeated n times
      */
     private final static String cloneString(String toClone, int n) {
-        if(toClone == null || (Integer) n == null)
+        if(toClone == null) {
             return null;
-        else if(toClone.equals("") || n <= 0)
+        } else if(toClone.equals("") || n <= 0) {
             return "";
-
-        String result = "";
-        for(int i = 0; i < n; i++)
-            result += toClone;
-        return result;
+        } else {
+            String result = "";
+            for (int i = 0; i < n; i++)
+                result += toClone;
+            return result;
+        }
     }
 
     /**
-     * Formats String to specified column width, then centers each // todo: better explanation
-     * line of text in the column.
+     * Formats String to specified column width, then centers each line of text
+     * in its column. See formatWidth for more information on how the String is
+     * formatted to the specified column width. Note: works best when columnWidth
+     * is an even number.
      *
      * @param text String to be center-formatted
      * @param columnWidth max number of characters per line
-     * @return a String where text of each line is centered in the column
+     * @return String containing original text where each line is centered in its column
      */
-    public final static String center(String text, int columnWidth) {
+    public final static String center(String text, int columnWidth) { // todo: check columnWidth?
         String result = "";
         // apply basic formatting
         String width_formatted = formatWidth(text, columnWidth);
@@ -163,15 +166,15 @@ public class TextFormat {
         }
         return result;
     }
-    // todo: documentation, parameter names
 
     /**
      * Formats String to specified column width, then right-justifies
-     * each line of text in the column.
+     * each line of text in the column. See formatWidth for more information
+     * on how the String is formatted to the specified column width.
      *
      * @param text String to be right-justified
      * @param columnWidth max number of characters per line
-     * @return a String where the text of each line has been right-justified // todo: better explanation
+     * @return String containing original text where text of each line is right-justified
      */
     public final static String rightJustify(String text, int columnWidth) {
         String result = "";
@@ -179,7 +182,7 @@ public class TextFormat {
         String width_formatted = formatWidth(text, columnWidth);
         String[] lines = width_formatted.split("\\n");
         for(int i = 0; i < lines.length; i++) {
-            lines[i] = lines[i].trim();
+            lines[i] = lines[i].trim(); // todo: do this without losing whitespace
             int leading_space = (columnWidth - lines[i].length());
             result += cloneString(" ", leading_space) + lines[i] + "\n";
         }
